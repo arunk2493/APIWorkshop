635c921086bf2bccd205b51ca34853d09c83a417
Arun Kumar <49553832+arunk2493@users.noreply.github.com>
2021-09-18 09:54:59 +0530

    Update GetOrderDetails.java

--- src/main/java/GetOrderDetails.java
+++ src/main/java/GetOrderDetails.java
@@ -5,5 +5,9 @@ import org.testng.annotations.Test;
 public class GetOrderDetails {
 //Sample test cases for reference
 // Assert the available value
+   @Test
+    public void validateGetResponseBody(){
+       
+    }
 
 }
