cfb4d6d62df57c640b2f187103d2d29ad5ff2c5c
Arun Kumar Kumaraswamy <arunkumarkumaraswamy@Aruns-MacBook-Pro.local>
2022-02-19 09:34:45 +0530

    Updated the json data

--- src/main/java/SampleTest.java
+++ src/main/java/SampleTest.java
@@ -20,50 +20,6 @@ public class SampleTest {
                 .body("category.id",Matchers.equalTo(0));
     }
 
-    //Assert the status code for Get call
-    @Test
-    public void validateGetResponse(){
-        RestAssured.given().get("https://petstore.swagger.io/v2/pet/1")
-                .then().log().body()
-                .assertThat().statusCode(200)
-                .body("id",Matchers.equalTo(1))
-                .body("category.id", Matchers.equalTo(0));
-
-    }
-    @Test
-    public void createOrderOnlyIfInventoryIsAvailable(){
-        /*Run the tests only if available inventory is greater than 0 and print the order ID*/
-
-        String inventory = given()
-                .baseUri("https://petstore.swagger.io")
-                .when()
-                .get("/v2/store/inventory")
-                .then()
-                .assertThat()
-                .statusCode(200)
-                .extract()
-                .response()
-                .asString();
-
-        JsonPath jsonPath = new JsonPath(inventory);
-        int available = jsonPath.getInt("available");
-
-        if (available> 0){
-            File loadOrderData = new File("src//main//resources//storeOrderData.json");
-            String response = given().baseUri("https://petstore.swagger.io").contentType(ContentType.JSON).body(loadOrderData)
-                    .when()
-                    .post("/v2/store/order")
-                    // THEN
-                    .then().log().body()
-                    .assertThat().statusCode(200)
-                    .extract()
-                    .response()
-                    .asString();
-
-            jsonPath = new JsonPath(response);
-            System.out.println("Order ID --------------> "+jsonPath.getLong("id"));
-        }
-    }
     @Test
     public void createPet(){
         // File loadPetData = new File("src//main//resources//newPetData.json");
